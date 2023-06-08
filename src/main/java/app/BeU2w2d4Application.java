f6f97886a67491a696e020c460c4f07911efdcf8
Andrea Di Paola <andr3a.dipaola@gmail.com>
2023-06-08 14:35:02 +0200

    first commit

---
+++ src/main/java/app/BeU2w2d4Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2w2d4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2w2d4Application.class, args);
+	}
+
+}
